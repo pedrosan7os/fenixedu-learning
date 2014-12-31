5c4e3af49230f47c11736ddf6eb9392910b73b05
Pedro Santos <pedro.san7os@gmail.com>
2014-12-31 13:53:12 +0000

    Removed dependency on online-tests. Evaluation lists works relying only on Evaluations abstract API

--- src/main/java/org/fenixedu/learning/domain/executionCourse/components/EvaluationsComponent.java
+++ src/main/java/org/fenixedu/learning/domain/executionCourse/components/EvaluationsComponent.java
@@ -1,34 +1,18 @@
 package org.fenixedu.learning.domain.executionCourse.components;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.fenixedu.academic.domain.Exam;
 import org.fenixedu.academic.domain.ExecutionCourse;
-import org.fenixedu.academic.domain.onlineTests.OnlineTest;
 import org.fenixedu.cms.domain.Page;
 import org.fenixedu.cms.domain.component.CMSComponent;
 import org.fenixedu.cms.domain.component.ComponentType;
-import org.fenixedu.learning.domain.executionCourse.ExecutionCourseSite;
 import org.fenixedu.cms.rendering.TemplateContext;
+import org.fenixedu.learning.domain.executionCourse.ExecutionCourseSite;
 
 @ComponentType(name = "Evaluations", description = "Evaluations for an Execution Course")
 public class EvaluationsComponent implements CMSComponent {
-
     @Override
     public void handle(Page page, TemplateContext componentContext, TemplateContext globalContext) {
         ExecutionCourse executionCourse = ((ExecutionCourseSite) page.getSite()).getExecutionCourse();
         globalContext.put("evaluations", executionCourse.getAssociatedEvaluationsSet());
         globalContext.put("comment", executionCourse.getComment());
-        globalContext.put("onlineTests", OnlineTest.readOnlineTestsByExecutionCourse(executionCourse));
-        globalContext.put("adHocEvaluations", executionCourse.getOrderedAssociatedAdHocEvaluations());
-        globalContext.put("projects", executionCourse.getAssociatedProjects());
-        globalContext.put("publishedExams", publishedExams(executionCourse));
-        globalContext.put("writtenTests", executionCourse.getAssociatedWrittenTests());
     }
-
-    private List<Exam> publishedExams(ExecutionCourse executionCourse) {
-        return executionCourse.getAssociatedExams().stream().filter(e -> e.isExamsMapPublished()).collect(Collectors.toList());
-    }
-
 }
